e1040f1a86cfdc496d24e20480e30dc5e7931955
Marcus NICOLAS <marcus.nicolas@protonmail.com>
2023-09-06 14:51:42 +0200

    my commit

--- InftyCat/LiftingProperty/TransFinComp.lean
+++ InftyCat/LiftingProperty/TransFinComp.lean
@@ -3,8 +3,6 @@ import Mathlib.SetTheory.Ordinal.Basic
 
 namespace CategoryTheory
 
-namespace MorphismProperty
-
 variable (C : Type u) [Category C]
 
 #synth LinearOrder Ordinal
@@ -17,28 +15,38 @@ variable (α : Ordinal)
 -- Ordinals are Quotient of Well orders. This picks one and the underlying category:
 #synth Category (Quotient.out α).α
 
-structure transfiniteChain where
-  F : (Quotient.out α).α ⥤ C
+structure Chain where
+  fonc : (Quotient.out α).α ⥤ C
   proof : sorry -- F is cocontinuous / preserves colimits
 
-structure transfiniteTower  where
-  F : ((Quotient.out α).α)ᵒᵖ ⥤ C
+structure Tower  where
+  fonc : ((Quotient.out α).α)ᵒᵖ ⥤ C
   proof : sorry -- F is continuous / preserves limits
 
--- Take two `transfiniteChain` with `α < β` then
 
-namespace transfiniteChain
+namespace Chain
 
-variable {C}
+variable {C}-- TODO: What's a colimiting cocone
 
-#check Limits.Cocone -- TODO: What's a colimiting cocone
 
-def extend {α β : Ordinal} (h : α ≤ β) (c : transfiniteChain C α)
-    (d : transfiniteChain C β) : Prop :=
-  sorry -- The functor `β ⥤ C` restricted to α extends `α ⥤ C`
+def restr {α β : Ordinal} (h : α ≤ β) (F : Chain C β) : Chain C α :=
+  sorry
 
-structure comp (c : transfiniteChain C α) (d : transfiniteChain C (α + 1)) : Prop where
-  p : extend (Ordinal.le_add_right α 1) c d
-  -- colimCone: ColimitingCocone d
+def extend {α β : Ordinal} (h : α ≤ β) (F : Chain C α) (G : Chain C β) : Prop :=
+  F = (restr h G)
+
+
+def coconeOfSucc (α : Ordinal) (F : Chain C (α+1)) :
+  Limits.Cocone (restr (Ordinal.le_add_right α 1) F).fonc :=
+  sorry
+
+structure compDiag (F : Chain C α) where
+  diag : Chain C (α+1)
+  p : extend (Ordinal.le_add_right α 1) F diag
+  ic : Limits.IsColimit (coconeOfSucc α diag)
+
+-- Should be a map from G(0) -> G(alpha)
+def comp {F : Chain C α} (G : compDiag F) :
+  sorry
 
 -- TODO: continue skeleton here.
