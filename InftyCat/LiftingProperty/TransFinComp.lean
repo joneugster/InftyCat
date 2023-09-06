438c107a0b48f774b6ae5f2c5751aaee33927fc8
Jon Eugster <eugster.jon@gmail.com>
2023-09-06 18:10:48 +0200

    def of chain and tower

--- InftyCat/LiftingProperty/TransFinComp.lean
+++ InftyCat/LiftingProperty/TransFinComp.lean
@@ -1,32 +1,51 @@
 import InftyCat.LiftingProperty.Basic
 import Mathlib.SetTheory.Ordinal.Basic
+import Mathlib.CategoryTheory.Limits.Preserves.Basic
 
 import Mathlib.Init.Algebra.Order
 import Mathlib.CategoryTheory.Category.Preorder
 
 namespace CategoryTheory
 
+open Limits
+
 variable (C : Type u) [Category C]
+
+
+variable (o : Ordinal.{v})
+#check (Quotient.out o).α
+
 
 
 -- Ordinals are Quotient of Well orders. This picks one and the underlying
 -- category: '(Quotient.out α).α'
 
-def Ordinal.toType (α : Ordinal.{v}) :=
-  { β : Ordinal.{v} // β < α}
+-- def Ordinal.toType (α : Ordinal.{v}) : Type (v + 1) :=
+--   {β : Ordinal.{v} // β < α}
+
+def Ordinal.toType : Type (v+1) := FullSubcategory (· < o)
+
+-- example (α β : Ordinal.{v}) (h : α < β) := sorry
+
+-- #check ordinalAsCategory o
+
+instance : Coe Ordinal.{v} (Type (v+1)) := ⟨ fun α => Ordinal.toType α ⟩
+
+--#check InducedCategory (Ordinal.{v}) (fun (β : Ordinal.toType o) => (β : Type (u+1)))
 
 #check Ordinal.toType
 
-instance : Coe Ordinal.{v} (Type (v+1)) := ⟨ fun α => α.toType ⟩
+class WellFoundedOrder (α : Type v) extends LinearOrder α, IsWellFounded α (· ≤ ·) where
 
+--instance : Preorder (Ordinal.toType α) := _
 
-structure Chain (α : Ordinal.{v}) where
-  fonc : { β : Ordinal.{v} // β < α}  ⥤ C
-  proof : sorry -- F is cocontinuous / preserves colimits
+structure Chain (α : Type v) [WellFoundedOrder α] where
+  F : α ⥤ C
+  proof : PreservesColimits F
 
-structure Tower (α : Ordinal.{v})  where
-  fonc : ((Quotient.out α).α)ᵒᵖ ⥤ C
-  proof : sorry -- F is continuous / preserves limits
+structure Tower (α : Type v) [WellFoundedOrder α] where
+  F : αᵒᵖ ⥤ C
+  proof : PreservesLimits F
 
 
 namespace Chain
