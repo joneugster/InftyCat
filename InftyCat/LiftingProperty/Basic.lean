1f2ab235ae0dad0441a5e0b8ab60a62490270332
hermanrohrbach <hermanrohrbach@gmail.com>
2023-09-07 18:26:03 +0200

    Update Basic.lean
    Opposite statements added

--- InftyCat/LiftingProperty/Basic.lean
+++ InftyCat/LiftingProperty/Basic.lean
@@ -32,11 +32,50 @@ Morphism Property `I`. -/
 def rightLiftingProperty (I : MorphismProperty C) : MorphismProperty C :=
   fun _ _ g => ∀ {x y} (i : x ⟶ y), I i → HasLiftingProperty i g
 
+theorem opLLP (I : MorphismProperty C) :
+    (rightLiftingProperty I.op).unop = leftLiftingProperty I := by
+  simp [leftLiftingProperty, rightLiftingProperty]
+  funext x y f  
+  ext
+  constructor
+  · intro hl a b i hi
+    apply HasLiftingProperty.unop
+    apply hl
+    assumption
+  · intro hr a_op b_op i_op hi_op
+    apply HasLiftingProperty.op
+    apply hr
+    assumption  
+
+#check HasLiftingProperty.iff_op
+
 class HasLeftLiftingProperty {x y : C} (I : MorphismProperty C) (f : x ⟶ y) : Prop where
   lift : (leftLiftingProperty I) f
 
 class HasRightLiftingProperty {x y : C} (I : MorphismProperty C) (g : x ⟶ y) : Prop where
   lift : (rightLiftingProperty I) g
+
+theorem op_of_LLPisRRP {x y : C} (I : MorphismProperty C) (f : x ⟶ y) : 
+    HasLeftLiftingProperty I.op f.op ↔ HasRightLiftingProperty I f := by
+  constructor
+  · intro h
+    rcases h with ⟨lift⟩ 
+    constructor
+    unfold leftLiftingProperty at lift
+    intro a b i hi
+    specialize lift i.op
+    apply HasLiftingProperty.unop
+    apply lift
+    simpa
+  · intro h
+    rcases h with ⟨lift⟩ 
+    constructor
+    unfold rightLiftingProperty at lift
+    intro a b i hi
+    specialize lift i.unop
+    apply HasLiftingProperty.op
+    apply lift
+    simpa
 
 /- This result makes it possible for us to dualize results on classes rlp(I)
    to results on classes llp(I) -/
